33cc10a2acf489414b9e42416125065b88e7d33c
Andrew Mason <andrewmcodes@protonmail.com>
2020-03-13 00:23:45 -0400

    try to reduce flash

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,27 @@
 /* eslint-disable */
 
 const React = require("react");
+
+const applyDarkModeClass = `
+(function() {
+  try {
+    var mode = localStorage.getItem('theme');
+    if (mode === 'dark') {
+			document.body.classList.add('dark');
+		}
+  } catch (e) {}
+})();
+`;
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  const script = React.createElement("script", {
+    dangerouslySetInnerHTML: {
+      __html: applyDarkModeClass
+    }
+  });
+  setPreBodyComponents([script]);
+};
+
 const GlobalContextProvider = require("./src/context/GlobalContextProvider")
   .default;
 
